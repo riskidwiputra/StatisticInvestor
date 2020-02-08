848394a7ed053a897be2e1859e4ae9a5cc8ebd43
riskidwiputra <52755256+riskidwiputra@users.noreply.github.com>
2020-02-08 12:24:14 +0700

    perbaikan

--- app/Config/Config.php
+++ app/Config/Config.php
@@ -12,9 +12,14 @@ class Config
 
 
 		define('DB_HOST', 'localhost');
-		define('DB_USER', 'root');
-		define('DB_PASS', '');
-		define('DB_NAME', 'db_investor'); 
+		define('DB_USER', 'u8127881_investor');
+		define('DB_PASS', 'Stream.investor');
+		define('DB_NAME', 'u8127881_investor');
+		
+// 		define('DB_HOST', 'localhost');
+// 		define('DB_USER', 'root');
+// 		define('DB_PASS', '');
+// 		define('DB_NAME', 'db_investor'); 
 
 		define('CONTROLLER', 'home');
 		define('METHOD', 'index');	
