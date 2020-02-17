61e3b78a8d3db0d416b2f78ec32a6955a7101951
riskidwiputra <52755256+riskidwiputra@users.noreply.github.com>
2020-02-17 15:45:38 +0700

    upload

---
+++ app/Config/Config.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace App\Config;
+
+class Config 
+{
+	public function __construct()
+	{
+		if (!(session_id())) session_start();
+		date_default_timezone_set('Asia/Jakarta'); 
+		ini_set( 'display_errors', 0 );
+
+
+		// define('DB_HOST', 'localhost');
+		// define('DB_USER', 'u8127881_investor');
+		// define('DB_PASS', '1nvestor12345');
+		// define('DB_NAME', 'u8127881_investor'); 
+		define('DB_HOST', 'localhost');
+		define('DB_USER', 'root');
+		define('DB_PASS', '');
+		define('DB_NAME', 'investor'); 
+// 		1nvestor12345
+
+		define('CONTROLLER', 'home');
+		define('METHOD', 'index');	
+		define('__CONFIG', 'portal'); 
+	}
+}
+
+
+new Config;
