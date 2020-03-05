08b93fc3771f21d7e1d27a40605b1a51c702ff43
riskidwiputra1250@gmail.com <riskidwiputra1250@gmail.com>
2020-03-05 16:25:52 +0700

    perbiakan

--- app/Controllers/ManagementController.php
+++ app/Controllers/ManagementController.php
@@ -14,5 +14,26 @@
             }
 
         }
+        public function AddDatamanagement()
+		{   
+            if(Session::check('superadmin') == true ){ 
+				$this->view('template/header', $data);
+				$this->view('pages/data_management/add_data_management',);
+				$this->view('template/footer');		
+			}else{
+				$this->view('login/login');
+            }
+		} 
+		public function Insert(){
+			if ( $this->model('Datamanagement_Model')->Insert($_POST) > 0) {
+                Flasher::setFlashSweet('successfully','Data successfully added','success');
+                redirect('/data-management');
+                exit;
+            } else { 
+                // Flasher::setFlashSweet('Failed','Data failed to add','warning'); 
+                redirect('/data-management');
+                exit;
+            }   
+		}
     
     }
