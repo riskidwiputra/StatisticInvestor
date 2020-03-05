f29ee60032072964b04fe86671b9238c3eac8a8b
riskidwiputra1250@gmail.com <riskidwiputra1250@gmail.com>
2020-03-05 16:21:50 +0700

    perbiakan

--- app/Controllers/ManagementController.php
+++ app/Controllers/ManagementController.php
@@ -4,15 +4,15 @@
 	{
         public function Management()
         {
-            var_dump("as");
-            // if(Session::check('superadmin') == true ){ 
-			// 	$data['content'] = $this->model('Datamanagement_Model')->select();
-			// 	$this->view('template/header', $data);
-			// 	$this->view('pages/data_management/data_management',$data);
-			// 	$this->view('template/footer');		
-			// }else{
-            //     $this->view('login/login');
-            // }
+            if(Session::check('superadmin') == true ){ 
+                // $data['content'] = $this->model('Datamanagement_Model')->select();
+                var_dump("asd");
+				// $this->view('template/header', $data);
+				// $this->view('pages/data_management/data_management',$data);
+				// $this->view('template/footer');		
+			}else{
+                $this->view('login/login');
+            }
 
         }
     
