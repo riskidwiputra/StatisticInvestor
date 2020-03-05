b2e0b3da52ad35b9048cad87ac5b7b7df29bcc1b
riskidwiputra1250@gmail.com <riskidwiputra1250@gmail.com>
2020-03-05 16:20:09 +0700

    update

--- app/Controllers/ManagementController.php
+++ app/Controllers/ManagementController.php
@@ -4,34 +4,16 @@
 	{
         public function Management()
         {
-            if(Session::check('superadmin') == true ){ 
-				$data['content'] = $this->model('Datamanagement_Model')->select();
-				$this->view('template/header', $data);
-				$this->view('pages/data_management/data_management',$data);
-				$this->view('template/footer');		
-			}else{
-                $this->view('login/login');
-            }
+            var_dump("as");
+            // if(Session::check('superadmin') == true ){ 
+			// 	$data['content'] = $this->model('Datamanagement_Model')->select();
+			// 	$this->view('template/header', $data);
+			// 	$this->view('pages/data_management/data_management',$data);
+			// 	$this->view('template/footer');		
+			// }else{
+            //     $this->view('login/login');
+            // }
+
         }
-        public function AddDatamanagement()
-		{   
-            if(Session::check('superadmin') == true ){ 
-				$this->view('template/header', $data);
-				$this->view('pages/data_management/add_data_management',);
-				$this->view('template/footer');		
-			}else{
-				$this->view('login/login');
-            }
-		} 
-		public function Insert(){
-			if ( $this->model('Datamanagement_Model')->insert($_POST) > 0) {
-                Flasher::setFlashSweet('successfully','Data successfully added','success');
-                redirect('/data-management');
-                exit;
-            } else { 
-                // Flasher::setFlashSweet('Failed','Data failed to add','warning'); 
-                redirect('/data-management');
-                exit;
-            }   
-		}
+    
     }
