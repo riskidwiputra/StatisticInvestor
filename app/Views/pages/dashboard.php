385c5060223f0f8f895b3d78f1f7a9b3519dc0f4
riskidwiputra1250@gmail.com <riskidwiputra1250@gmail.com>
2020-03-05 14:25:56 +0700

    perbaikan

--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -1,7 +1,11 @@
 <div class="main-panel ">
           <div class="content-wrapper">
         
+          <?php Flasher::flash();?>
           
+        
+
+        
 
             <div class="page-header">
               <h3 class="page-title">
@@ -96,75 +100,172 @@
               </nav>
             </div>
             <div class="row">
-              <div class="col-12 grid-margin">
+            <div class="col-12 grid-margin">
                 <div class="card">
-                  <div class="card-body">
-                    <h4 class="card-title">Daily</h4>
-                    <div class="media">
-                          <img class="align-self-start mr-3" src="<?=asset('assets/images/r6s-shiftingtides-bg-header_358641.jpg');?>" alt="Generic placeholder image" style="width:200px;">
-                          <div class="media-body d-none d-xl-block">
-                            <h5 class="mt-0">Rainbow Six: Siege Operation Shifting Tides Tambah Operator dan Rombak Bentuk Map</h5>
-                            <p>Berasal dari India, Kali diceritakan sebagai pemimpin pasukan militer privat milik sebuah perusahaan. Mewakili kata “Tides”, Operator dengan medium armor/medium speed ini membawa senjata yang sangat mematikan, CSRX 300 rifle. Senjata ini jadi mematikan karena bisa langsung menumbangkan musuh dengan satu kali tembak saja. Gadget ini berbentuk piringan, dapat dilempar, dan akan menempel ke permukaan di tempat Mag-NET dilempar. Setelahnya, semua proyektil yang dilempar Attacker akan ditarik oleh Mag-NET jika melewati area sekitar Mag-NET tertempel. Satu Mag-NET hanya bisa menarik satu proyektil saja, d....<u>Selengkapnya</u></p>
-                          </div> 
+                    <div class="card-body">
+                        <h4 class="card-title">Daily</h4>
+                        <?php if($data['daily']){ ?>
+                        <?php foreach ($data['daily'] as $rows) { ?>
+                        <div class="media pt-4">
+                            <img class="align-self-start mr-3" src="<?=path('path_portal_News');?><?= $rows['image'] ?>" alt="Generic placeholder image" style="width:200px; height:100px;">
+                            <div class="media-body d-none d-xl-block">      
+                                <div class="pb-2">
+                                </div>
+                                <h5 class="mt-0"><?= strtoupper($rows['title']) ?>
+                                </h5>
+                                <p style="margin-top:-15px;">
+                                <?php  
+                                $text = htmlspecialchars_decode($rows['content']);
+                                if ( str_word_count($rows['content']) > 60 ){
+                                echo  substr($text,0,250)." .... <a href='".url('news/').$rows['url']."'><u>Selengkapnya</u></a>  " ;
+                                } else {
+                                echo $text."... <a href='".url('news/').$rows['url']."'><u>Selengkapnya</u></a>";
+                                }?>
+                                </p>
+                            </div> 
                         </div>
-                        <div class="row d-xl-none mt-2">
-                            <div class="col-12">
-                                <h5>Rainbow Six: Siege Operation Shifting Tides Tambah Operator dan Rombak Bentuk Map</h5>
-                                <p>Berasal dari India, Kali diceritakan sebagai pemimpin pasukan militer privat milik sebuah perusahaan. Mewakili kata “Tides”, Operator dengan medium armor....<u>Selengkapnya</u></p>
-                            </div>
-                        </div>
-                        <div class="media">
-                          <img class="align-self-start mr-3" src="<?=asset('assets/images/1.jpg');?>" alt="Generic placeholder image" style="width:200px;">
-                          <div class="media-body d-none d-xl-block">
-                            <h5 class="mt-0">Tinggalkan AR, Riot Games Gunakan Teknologi Hologram Pada Pembukaan Final Worlds 2019</h5>
-                            <p>Dimulai dari penampilan Valerie Broussard yang membawakan lagu Awaken, True Damage yang melakoni debutnya dengan lagu GIANTS, Chrissy Costanza dan Cailin....<u>Selengkapnya</u></p>
-                          </div>
-                        </div>
-                        <div class="row d-xl-none mt-2">
-                            <div class="col-12">
-                                <h5>Tinggalkan AR, Riot Games Gunakan Teknologi Hologram Pada Pembukaan Final Worlds 2019</h5>
-                                <p>Dimulai dari penampilan Valerie Broussard yang membawakan lagu Awaken, True Damage yang melakoni debutnya dengan lagu GIANTS, Chrissy Costanza dan Cailin Russo yang....<u>Selengkapnya</u></p>
-                            </div>
-                        </div> 
-                  </div>
+                        <?php } ?>
+                        <nav aria-label="Page navigation example">
+                            <ul class="pagination text-primary justify-content-center ">
+                                <?php if (isset($data['page-daily'])) {
+                                if ($data['page-daily'] == 1) { ?>
+                                <li class="page-item"><a class="page-link" ><i class="fa fa-angle-left"></i></a></li>
+                                <?php  
+                                }else {
+                                $link_prev = ($data['page-daily'] > 1)? $data['page-daily'] - 1 : 1;
+                                ?>
+                                <li class="page-item"><a class="page-link" href="<?= BASEURL ?>/news-daily/<?= $link_prev ?>"><i class="fa fa-angle-left"></i></a></li>
+                                <?php
+                                }
+                                for ($i= $data['start_number']; $i <= $data['end_number']; $i++) { 
+                                $link_active = ($data['page-daily'] == $i)? 'active' : '';
+                                ?>
+                                <li class="page-item <?= $link_active ?>"><a class="page-link" href="<?= BASEURL ?>/news-daily/<?= $i ?>"><?= $i ?></a></li>
+                                <?php
+                                }
+                                if ($data['page-daily'] == $data['jumlahHalaman']) {
+                                ?>
+
+                                <li class="page-item"><a class="page-link"><i class="fa fa-angle-right"></i></a></li>
+                                <?php
+                                }else{
+                                $link_next = ($data['page-daily'] < $data['jumlahHalaman'])? $data['page-daily'] + 1 : $data['jumlahHalaman'];
+                                ?>
+                                <li class="page-item"><a class="page-link" href="<?= BASEURL ?>/news-daily/<?= $link_next ?>""><i class="fa fa-angle-right"></i></a></li>
+                                <?php
+                                }
+                                }else{?>
+                                <li class="page-item"><a class="page-link"><i class="fa fa-angle-left"></i></a></li>
+                                <?php
+                                for ($i=1; $i <= $data['jumlahHalamanDaily']; $i++) { 
+                                ?>
+                                <li class="page-item"><a class="page-link" href="<?= BASEURL ?>/news-daily/<?= $i ?>"><?= $i ?></a></li>
+                                <?php    
+                                }
+                                ?>
+                                <?php
+                                if ($data['jumlahHalamanDaily'] == 1) {
+                                ?>
+                                <li class="page-item"><a class="page-link"><i class="fa fa-angle-right"></i></a></li>
+                                <?php
+                                }else{
+                                ?>
+                                <li class="page-item"><a class="page-link" href="<?= BASEURL ?>/news-daily/2"><i class="fa fa-angle-right"></i></a></li>
+                                <?php
+                                }
+                                }
+                                ?>
+                            </ul>
+                        </nav>
+                        <?php } ?>
+                    </div>
                 </div>
-              </div>
             </div>
-            <div class="row">
-              <div class="col-12 grid-margin stretch-card">
+        </div>
+        <div class="row">
+            <div class="col-12 grid-margin stretch-card">
                 <div class="card">
-                  <div class="card-body">
-                    <h4 class="card-title">Monthly</h4>
-                    <div class="media">
-                          <img class="align-self-start mr-3" src="<?=asset('assets/images/OG-Juara-The-International-Dota-2-TI9-Sejarah-Baru-Tercipta.png');?>" alt="Generic placeholder image" style="width:200px;">
-                          <div class="media-body d-none d-xl-block">
-                            <h5 class="mt-0">OG Juara The International 2019 (TI9), Sejarah Baru Tercipta!</h5>
-                            <p>OG akhirnya menjadi juara di turnamen The International Dota 2 tahun 2019 (TI9). Skuad dari Eropa ini menyelesaikan kisah Cinderella keduanya di turnamen Dota 2 terbesar di dunia dengan mengalahkan Team Liquid di grand final acara TI9 tersebut. Kemenangan ini mencatatkan laju dominan team OG di ajang TI9 dan mendapatkan hadiah sebesar $15.578.510 atau kurang lebih setara dengan Rp. 222 Milliar!<u>Selengkapnya</u></p>
-                          </div>
+                    <div class="card-body">
+                        <h4 class="card-title">Monthly</h4>
+                        <?php if($data['monthly']){ ?>
+                        <?php foreach ($data['monthly'] as $rows) { ?>
+                        <div class="media pt-4">
+                        <img class="align-self-start mr-3" src="<?=path('path_portal_News');?><?= $rows['image'] ?>" alt="Generic placeholder image" style="width:200px; height:100px;">
+                            <div class="media-body d-none d-xl-block">
+                                <h5 class="mt-0"><?= strtoupper($rows['title']) ?>
+                                                                  </h5>
+                                <p style="margin-top:-15px;">
+                                    <?php  
+                                    $text = htmlspecialchars_decode($rows['content']);
+                                    if ( str_word_count($rows['content']) > 60 ){
+                                    echo  substr($text,0,250)." .... <a href='".url('news/').$rows['url']."'><u>Selengkapnya</u></a>  " ;
+                                    } else {
+                                    echo $text;
+                                    }
+                                    ?>
+                                </p>
+                            </div> 
                         </div>
-                        <div class="row d-xl-none mt-2">
-                            <div class="col-12">
-                                <h5>OG Juara The International 2019 (TI9), Sejarah Baru Tercipta!</h5>
-                                <p>OG akhirnya menjadi juara di turnamen The International Dota 2 tahun 2019 (TI9). Skuad dari Eropa ini menyelesaikan kisah Cinderella keduanya di turnamen Dota 2 terbe....<u>Selengkapnya</u></p>
-                            </div>
-                        </div>
-                        <div class="media">
-                          <img class="align-self-start mr-3" src="<?=asset('assets/images/032031100_1564393927-MLBB3.jpg');?>" alt="Generic placeholder image" style="width:200px;">
-                          <div class="media-body d-none d-xl-block">
-                            <h5 class="mt-0">11 Cosplay Mobile Legends (ML) yang Akan Buatmu Mimisan</h5>
-                            <p>Mobile Legends saat ini adalah game Moba terdepan di Indonesia. Deretan pro player, turnamen tingkat dunia hingga berbagai perlombaan cosplay membuat popularitas game Mobile Legends (ML) ini semakin meningkat dari waktu ke waktu. Tidak mengherankan para cosplayer berlomba lomba berkreasi untuk membuat karakternya menjadi semakin hidup....<u>Selengkapnya</u></p>
-                          </div>
-                        </div>
-                        <div class="row d-xl-none mt-2">
-                            <div class="col-12">
-                                <h5>11 Cosplay Mobile Legends (ML) yang Akan Buatmu Mimisan</h5>
-                                <p>Mobile Legends saat ini adalah game Moba terdepan di Indonesia. Deretan pro player, turnamen tingkat dunia hingga berbagai perlombaan cosplay membuat popularitas game Mobile Legends ....<u>Selengkapnya</u></p>
-                            </div>
-                        </div> 
-                  </div>
+                        <?php } ?>
+                        <nav aria-label="Page navigation example">
+                            <ul class="pagination text-primary justify-content-center ">
+                                <?php if (isset($data['page-monthly'])) {
+                                if ($data['page-monthly'] == 1) { ?>
+                                <li class="page-item"><a class="page-link" ><i class="fa fa-angle-left"></i></a></li>
+                                <?php  
+                                }else {
+                                $link_prev = ($data['page-monthly'] > 1)? $data['page-monthly'] - 1 : 1;
+                                ?>
+                                <li class="page-item"><a class="page-link" href="<?= BASEURL ?>/news-monthly/<?= $link_prev ?>"><i class="fa fa-angle-left"></i></a></li>
+                                <?php
+                                }
+                                for ($i= $data['start_number']; $i <= $data['end_number']; $i++) { 
+                                $link_active = ($data['page-monthly'] == $i)? 'active' : '';
+                                ?>
+                                <li class="page-item <?= $link_active ?>"><a class="page-link" href="<?= BASEURL ?>/news-monthly/<?= $i ?>"><?= $i ?></a></li>
+                                <?php
+                                }
+                                if ($data['page-monthly'] == $data['jumlahHalaman']) {
+                                ?>
+
+                                <li class="page-item"><a class="page-link"><i class="fa fa-angle-right"></i></a></li>
+                                <?php
+                                }else{
+                                $link_next = ($data['page-monthly'] < $data['jumlahHalaman'])? $data['page-monthly'] + 1 : $data['jumlahHalaman'];
+                                ?>
+                                <li class="page-item"><a class="page-link" href="<?= BASEURL ?>/news-monthly/<?= $link_next ?>""><i class="fa fa-angle-right"></i></a></li>
+                                <?php
+                                }
+                                }else{?>
+                                <li class="page-item"><a class="page-link"><i class="fa fa-angle-left"></i></a></li>
+                                <?php
+                                for ($i=1; $i <= $data['jumlahHalamanMonthly']; $i++) { 
+                                ?>
+                                <li class="page-item"><a class="page-link" href="<?= BASEURL ?>/news-monthly/<?= $i ?>"><?= $i ?></a></li>
+                                <?php    
+                                }
+                                ?>
+                                <?php
+                                if ($data['jumlahHalamanMonthly'] == 1) {
+                                ?>
+                                <li class="page-item"><a class="page-link"><i class="fa fa-angle-right"></i></a></li>
+                                <?php
+                                }else{
+                                ?>
+                                <li class="page-item"><a class="page-link" href="<?= BASEURL ?>/news-monthly/2"><i class="fa fa-angle-right"></i></a></li>
+
+
+                                <?php
+                                }
+                                }
+                                ?>
+                            </ul>
+                        </nav>
+                        <?php } ?>
+                    </div>
                 </div>
-              </div>
             </div>
+        </div>
             <div class="page-header">
               <h6 class="page-title" style="font-size:16px;">
                 <span class="page-title-icon bg-gradient-primary text-white mr-2">
